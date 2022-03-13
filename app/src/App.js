d1334eb0244ac9fff361b7c661c1f6b65b39b679
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-03-12 18:50:09 -0800

    commit

---
+++ app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello Dojo! </h1>
+      <h3> Things I need to do! </h3>
+      <ul>
+        <li> Learn React. </li>
+        <li> Master React. </li>
+        <li> Get a job working on React Applications. </li>
+        <li> Keep learning and becoming better! </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
